a14d2fc36e33c9d425d0cb58bced61f39bdef9ea
ryoo0930 <ryoo0930@naver.com>
2025-09-20 17:43:43 +0900

    add entity logic

---
+++ src/main/java/org/newdawn/spaceinvaders/entity/strategy/DownThenRightMovementStrategy.java
@@ -0,0 +1,41 @@
+package org.newdawn.spaceinvaders.entity.strategy;
+
+import org.newdawn.spaceinvaders.entity.Entity;
+
+public class DownThenRightMovementStrategy implements InnerMovementStrategy {
+    private enum State {
+        MOVING_DOWN,
+        MOVING_RIGHT
+    }
+
+    private State currentState = State.MOVING_DOWN;
+    private final double moveSpeed;
+
+    public DownThenRightMovementStrategy(double moveSpeed){
+        this.moveSpeed = moveSpeed;
+    }
+
+    @Override
+    public void move(Entity entity, long delta){
+        switch (currentState) {
+            case MOVING_DOWN:
+                if(entity.getVerticalMovement() == 0){
+                    entity.setVerticalMovement(moveSpeed);
+                    entity.setHorizontalMovement(0);
+                }
+                if(entity.getY() > 200){
+                    currentState = State.MOVING_RIGHT;
+                    entity.setVerticalMovement(0);
+                }
+                break;
+        
+            case MOVING_RIGHT:
+                if(entity.getHorizontalMovement() == 0){
+                    entity.setHorizontalMovement(moveSpeed);
+                    entity.setVerticalMovement(0);
+                }
+                break;
+        }
+        entity.move(delta);
+    }
+}
--- src/main/java/org/newdawn/spaceinvaders/entity/strategy/InnerMovementStrategy.java
+++ src/main/java/org/newdawn/spaceinvaders/entity/strategy/InnerMovementStrategy.java
@@ -5,5 +5,4 @@ import org.newdawn.spaceinvaders.entity.Entity;
 /** AlienEntity의 특정 움직임을 추가하기 위한 인터페이스 */
 public interface InnerMovementStrategy {
     void move(Entity entity, long delta);
-
 }
