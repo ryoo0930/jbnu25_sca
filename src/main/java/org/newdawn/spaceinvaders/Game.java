7c6f524593c789149db40a3b55334cc7b2774c1c
ryoo0930 <ryoo0930@naver.com>
2025-09-20 14:10:54 +0900

    Add mainMenu

--- src/main/java/org/newdawn/spaceinvaders/Game.java
+++ src/main/java/org/newdawn/spaceinvaders/Game.java
@@ -16,7 +16,7 @@ import javax.swing.JPanel;
 
 /**
  * The main hook of our game. This class with both act as a manager
- * for the display and central mediator for the game logic. 
+ * for the display and central mediator for the game logic.
  * 
  * Display management will consist of a loop that cycles round all
  * entities in the game asking them to move and then drawing them
@@ -29,16 +29,17 @@ import javax.swing.JPanel;
  * 
  * @author Kevin Glass
  */
-public class Game extends Canvas 
-{
+public class Game extends Canvas {
 	/** 게임 상태. 현재 Game class는 화면만 보여줄 뿐, GameState를 통해 화면의 구성을 변경시킵니다. */
-	private enum GameState{ MAIN_MENU, GAME_PLAY }
+	private enum GameState {
+		MAIN_MENU, GAME_PLAY, SCORE, OPTION, EXIT
+	}
 
 	/** The stragey that allows us to use accelerate page flipping */
 	private BufferStrategy strategy;
 	/** True if the game is currently "running", i.e. the game loop is looping */
 	private boolean gameRunning = true;
-	
+
 	/** True if we're holding up game play until a key has been pressed */
 	private boolean waitingForKeyPress = true;
 	/** Ture if the up cursor key is currently pressed */
@@ -51,7 +52,10 @@ public class Game extends Canvas
 	private boolean rightPressed = false;
 	/** True if we are firing */
 	private boolean firePressed = false;
-	/** True if game logic needs to be applied this loop, normally as a result of a game event */
+	/**
+	 * True if game logic needs to be applied this loop, normally as a result of a
+	 * game event
+	 */
 	private boolean logicRequiredThisLoop = false;
 	/** The last time at which we recorded the frame rate */
 	private long lastFpsTime;
@@ -65,36 +69,34 @@ public class Game extends Canvas
 	/** 메인 메뉴 */
 	private MainMenu mainMenu;
 
-
-
 	/** 게임 시작 시 처음으로 보여줄 화면 */
-	private GameState currenGameState = GameState.MAIN_MENU;
-	
+	private GameState currentGameState = GameState.MAIN_MENU;
+
 	/**
 	 * Construct our game and set it running.
 	 */
 	public Game() {
 		// create a frame to contain our game
 		container = new JFrame("Space Invaders 102");
-		
+
 		// get hold the content of the frame and set up the resolution of the game
 		JPanel panel = (JPanel) container.getContentPane();
-		panel.setPreferredSize(new Dimension(800,600));
+		panel.setPreferredSize(new Dimension(800, 600));
 		panel.setLayout(null);
-		
+
 		// setup our canvas size and put it into the content of the frame
-		setBounds(0,0,800,600);
+		setBounds(0, 0, 800, 600);
 		panel.add(this);
-		
+
 		// Tell AWT not to bother repainting our canvas since we're
 		// going to do that our self in accelerated mode
 		setIgnoreRepaint(true);
-		
-		// finally make the window visible 
+
+		// finally make the window visible
 		container.pack();
 		container.setResizable(false);
 		container.setVisible(true);
-		
+
 		// add a listener to respond to the user closing the window. If they
 		// do we'd like to exit the game
 		container.addWindowListener(new WindowAdapter() {
@@ -102,11 +104,11 @@ public class Game extends Canvas
 				System.exit(0);
 			}
 		});
-		
+
 		// add a key input system (defined below) to our canvas
 		// so we can respond to key pressed
 		addKeyListener(new KeyInputHandler());
-		
+
 		// request the focus so key events come to us
 		requestFocus();
 
@@ -114,19 +116,18 @@ public class Game extends Canvas
 		// to manage our accelerated graphics
 		createBufferStrategy(2);
 		strategy = getBufferStrategy();
-		
-		// MainMenu 객체 생성
+
 		mainMenu = new MainMenu();
 	}
-	
-	/**
-	 * Notification from a game entity that the logic of the game
-	 * should be run at the next opportunity (normally as a result of some
-	 * game event)
-	 */
-	public void updateLogic() {
-		logicRequiredThisLoop = true;
-	}
+
+
+	public void updateLogic() { }
+	public void removeEntity(Object entity) { }
+	public void notifyDeath() { }
+	public void notifyWin() { }
+	public void notifyAlienKilled() { }
+	public void tryToFire() { }
+
 
 	/**
 	 * The main game loop. This loop is running during all game
@@ -141,7 +142,7 @@ public class Game extends Canvas
 	 */
 	public void gameLoop() {
 		long lastLoopTime = SystemTimer.getTime();
-		
+
 		// keep looping round til the game ends
 		while (gameRunning) {
 			// work out how long its been since the last update, this
@@ -153,52 +154,72 @@ public class Game extends Canvas
 			// update the frame counter
 			lastFpsTime += delta;
 			fps++;
-			
+
 			// update our FPS counter if a second has passed since
 			// we last recorded
 			if (lastFpsTime >= 1000) {
-				container.setTitle(windowTitle+" (FPS: "+fps+")");
+				container.setTitle(windowTitle + " (FPS: " + fps + ")");
 				lastFpsTime = 0;
 				fps = 0;
 			}
-			
-			// Get hold of a graphics context for the accelerated 
+
+			// Get hold of a graphics context for the accelerated
 			// surface and blank it out
 			Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
 			g.setColor(Color.black);
-			g.fillRect(0,0,800,600);
-			
-			//화면 상태 변경
-			switch (currenGameState) {
+			g.fillRect(0, 0, 800, 600);
+
+			// 화면 상태 변경
+			switch (currentGameState) {
 				case MAIN_MENU:
 					mainMenu.update();
 					mainMenu.draw(g);
 					break;
-			
-			}
 
+			}
 
 			// finally, we've completed drawing so clear up the graphics
 			// and flip the buffer over
 			g.dispose();
 			strategy.show();
-			
-			
+
 			// we want each frame to take 10 milliseconds, to do this
 			// we've recorded when we started the frame. We add 10 milliseconds
-			// to this and then factor in the current time to give 
+			// to this and then factor in the current time to give
 			// us our final value to wait for
-			SystemTimer.sleep(lastLoopTime+10-SystemTimer.getTime());
+			SystemTimer.sleep(lastLoopTime + 10 - SystemTimer.getTime());
 		}
 	}
-	
+
+	/** 메인 메뉴 선택 로직 추가 */
+	private void selectMenuOption() {
+		int selection = mainMenu.getSelection();
+		switch (selection) {
+			case 0:
+				System.out.println("start");
+				currentGameState = GameState.GAME_PLAY;
+				break;
+			case 1:
+				System.out.println("score");
+				currentGameState = GameState.SCORE;
+				break;
+			case 2:
+				System.out.println("option");
+				currentGameState = GameState.OPTION;
+				break;
+			case 3:
+				System.exit(0);
+				break;
+		}
+	}
+
 	/**
 	 * A class to handle keyboard input from the user. The class
-	 * handles both dynamic input during game play, i.e. left/right 
+	 * handles both dynamic input during game play, i.e. left/right
 	 * and shoot, and more static type input (i.e. press any key to
 	 * continue)
 	 * 
-	 * This has been implemented as an inner class more through 
+	 * This has been implemented as an inner class more through
 	 * habbit then anything else. Its perfectly normal to implement
 	 * this as seperate class if slight less convienient.
 	 * 
@@ -207,25 +228,33 @@ public class Game extends Canvas
 	private class KeyInputHandler extends KeyAdapter {
 		/** The number of key presses we've had while waiting for an "any key" press */
 		private int pressCount = 1;
-		
+
 		/**
 		 * Notification from AWT that a key has been pressed. Note that
 		 * a key being pressed is equal to being pushed down but *NOT*
 		 * released. Thats where keyTyped() comes in.
 		 *
-		 * @param e The details of the key that was pressed 
+		 * @param e The details of the key that was pressed
 		 */
 		public void keyPressed(KeyEvent e) {
-			// if we're waiting for an "any key" typed then we don't 
-			// want to do anything with just a "press"
-			if (waitingForKeyPress) {
-				return;
+
+			/** 메인 메뉴 키보드 처리 */
+			if (currentGameState == GameState.MAIN_MENU) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					mainMenu.moveUp();
+				}
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					mainMenu.moveDown();
+				}
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					selectMenuOption();
+				}
 			}
-			
-			if(e.getKeyCode() == KeyEvent.VK_UP){
+
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
 				upPressed = true;
 			}
-			if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 				downPressed = true;
 			}
 			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
@@ -237,20 +266,20 @@ public class Game extends Canvas
 			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 				firePressed = true;
 			}
-		} 
-		
+		}
+
 		/**
 		 * Notification from AWT that a key has been released.
 		 *
-		 * @param e The details of the key that was released 
+		 * @param e The details of the key that was released
 		 */
 		public void keyReleased(KeyEvent e) {
-			// if we're waiting for an "any key" typed then we don't 
+			// if we're waiting for an "any key" typed then we don't
 			// want to do anything with just a "released"
 			if (waitingForKeyPress) {
 				return;
 			}
-			
+
 			if (e.getKeyCode() == KeyEvent.VK_UP) {
 				upPressed = false;
 			}
@@ -272,7 +301,7 @@ public class Game extends Canvas
 		 * Notification from AWT that a key has been typed. Note that
 		 * typing a key means to both press and then release it.
 		 *
-		 * @param e The details of the key that was typed. 
+		 * @param e The details of the key that was typed.
 		 */
 		public void keyTyped(KeyEvent e) {
 			// if we're waiting for a "any key" type then
@@ -283,23 +312,24 @@ public class Game extends Canvas
 			if (waitingForKeyPress) {
 				if (pressCount == 1) {
 					// since we've now recieved our key typed
-					// event we can mark it as such and start 
+					// event we can mark it as such and start
 					// our new game
 					waitingForKeyPress = false;
-					//<==================================================================== gameStart();
+					// <====================================================================
+					// gameStart();
 					pressCount = 0;
 				} else {
 					pressCount++;
 				}
 			}
-			
+
 			// if we hit escape, then quit the game
 			if (e.getKeyChar() == 27) {
 				System.exit(0);
 			}
 		}
 	}
-	
+
 	/**
 	 * The entry point into the game. We'll simply create an
 	 * instance of class which will start the display and game
