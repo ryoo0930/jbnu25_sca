05e1503611d34a1b3a05a6ee096a2819d7e56edd
ryoo0930 <ryoo0930@naver.com>
2025-09-21 09:34:46 +0900

    Stage interface

---
+++ src/main/java/org/newdawn/spaceinvaders/stage/Stage.java
@@ -0,0 +1,10 @@
+package org.newdawn.spaceinvaders.stage;
+
+import java.util.ArrayList;
+import org.newdawn.spaceinvaders.Game;
+import org.newdawn.spaceinvaders.entity.Entity;
+
+public interface Stage {
+    void initEntities(Game game, ArrayList<Entity> entities);
+    int getAlienCount();
+}
