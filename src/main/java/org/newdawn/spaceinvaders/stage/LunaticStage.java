811564c6b670b9def03a5741802dde79d1a4d283
ryoo0930 <ryoo0930@naver.com>
2025-09-21 09:39:24 +0900

    Lunatic Stage add

---
+++ src/main/java/org/newdawn/spaceinvaders/stage/LunaticStage.java
@@ -0,0 +1,27 @@
+package org.newdawn.spaceinvaders.stage;
+
+import java.util.ArrayList;
+import org.newdawn.spaceinvaders.Game;
+import org.newdawn.spaceinvaders.entity.AlienEntity;
+import org.newdawn.spaceinvaders.entity.Entity;
+
+public class LunaticStage implements Stage {
+    private int alienCount;
+
+    @Override
+    public void initEntities(Game game, ArrayList<Entity> entities) {
+        alienCount = 0;
+        for (int row = 0; row < 5; row++) {
+            for (int x = 0; x < 12; x++) {
+                Entity alien = new AlienEntity(game, 100 + (x * 50), (50) + row * 30);
+                entities.add(alien);
+                alienCount++;
+            }
+        }
+    }
+
+    @Override
+    public int getAlienCount() {
+        return alienCount;
+    }
+}
