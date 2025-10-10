79282987f418f9fce08ea3f5bc4b49b29f04a577
ryoo0930 <ryoo0930@naver.com>
2025-10-10 15:55:49 +0900

    애니메이션 처리 변경

--- src/main/java/org/newdawn/spaceinvaders/entity/AlienEntity.java
+++ src/main/java/org/newdawn/spaceinvaders/entity/AlienEntity.java
@@ -34,13 +34,13 @@ public class AlienEntity extends Entity {
 	 * @param y The intial y location of this alient
 	 */
 	public AlienEntity(Game game,int x,int y) {
-		super("sprites/alien.gif",x,y);
+		super("sprites/Boss1.gif",x,y);
 		
 		// setup the animatin frames
 		frames[0] = sprite;
-		frames[1] = SpriteStore.get().getSprite("sprites/alien2.gif");
+		frames[1] = SpriteStore.get().getSprite("sprites/Boss1.gif");
 		frames[2] = sprite;
-		frames[3] = SpriteStore.get().getSprite("sprites/alien3.gif");
+		frames[3] = SpriteStore.get().getSprite("sprites/Boss1.gif");
 		
 		this.game = game;
 		dx = -moveSpeed;
